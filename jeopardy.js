e2c5ff3d1d1eede73abb75a4c9ee88c4b7c89ee2
Jonathan Wilson <jonathansw@comcast.net>
2023-05-19 16:15:46 -0400

    second version

--- jeopardy.js
+++ jeopardy.js
@@ -21,28 +21,27 @@
 //    ...
 //  ]
 
-let cats = [];
+let catagory = [];
 gameObj = {}
 let catRes = [];
-let cCount = [];
-
-
 
 const reStart =  document.querySelector('#restart')//for restart button
 reStart.addEventListener('click', function(){
-    setupAndStart()
+    load()
 })
 
-function setupAndStart() {
-    sessionStorage.setItem("reloading", "true");
-    document.location.reload();
+
+function load() {
+    sessionStorage.setItem("reloading", "true");//loads 'reloading' into session storage
+    document.location.reload();//reloads page
     
 }
 window.onload = function() {
     let reloading = sessionStorage.getItem("reloading");
     if (reloading) {
         sessionStorage.removeItem("reloading");
-        getCategoryIds();
+        showLoadingView();//function that loads spinner
+        getCategoryIds();//starts process to load game table
     }
 }
 
@@ -50,56 +49,44 @@ async function getCategoryIds() {
 
 
     const res =  await axios.get('https://jservice.io/api/categories', {params: {count: 100}})//this gets the catagorys
-   
+    let catagoryIds = res.data.map(f => f.id);
+    let clueCount = res.data.map(f => f.clues_count);
+
    //selects 6 catagorys at random for the game
    for (let y = 0; y < 6; y++){
-    ran = Math.floor(Math.random()* 100)
-    if(res.data[ran].clues_count>4){//if the clues dont have at least five Q and A's another catagory is selected
-    cats[y]= res.data[ran];}
-    else{y--} 
-   
+    ran = Math.floor(Math.random()* catagoryIds.length)
+    if(clueCount[ran]>4){//if the selectted clues dont have at least five Q and A's another catagory is selected
+       
+        catagory[y]= catagoryIds[ran];}
     
-   // console.log(cats[y].clues_count)//cats is an array of 6 catagorys.  the questions have not been selected
-   }
-  
-   getCatorgories(cats)
-   buildBoard(cats) 
+    else{y--
+
+    }
+}
+  getCatorgories(catagory)//this gets the quesitons and answers
+   
 
 }
 
 
-async function getCatorgories(cats) {// this function get the catagories
+async function getCatorgories(catagory) {// this function getS the quesitons and answers
  
 
     for(let x = 0; x<6; x++){//gets the six catagories for the board
-      catRes[x] =  await axios.get('https://jservice.io/api/category', {params: {id: cats[x].id}});
-    }
-// buildCatObj(catRes);//builds the catObj for the game, sends over the catagories to get the object
-       buildQuestions()//initializes the game board with the question marks
+      catRes[x] =  await axios.get('https://jservice.io/api/category', {params: {id: catagory[x]}
+    });
+    
+    
       
-      // getFiveClues(cats)
-      cCount=getCcount(0);
-      loadGameObject(cCount)
+    }
+    
+fillTable(catRes)//this loads the top row, or the catagories.  Labeled header
+let gameObj = getCategory(catRes)
+buildQuestions()//initializes and builds the game board with the question marks
+    
      }
 
-     function buildBoard(cats){
-        const header = document.querySelector('#container')
-        hContainer = document.createElement('div');
-        hContainer.classList.add('headerBlock')
-        
-         for(x=0; x<6; x++){
-        const square =  document.createElement('div');
-        square.classList.add('catagory');
-        square.innerHTML = cats[x].title;
-        
-        hContainer.append(square)
-        header.append(hContainer)
-         }
-    }
-
-
-
-function buildQuestions(){//builds the initial question marks for the game
+    function buildQuestions(){//builds the initial question marks for the game
     const block = document.querySelector('#container')
     const questionContainer = document.createElement('div')
     questionContainer.classList.add('questionContainer')
@@ -118,109 +105,15 @@ function buildQuestions(){//builds the initial question marks for the game
     col.append(square)
     columns++;        
     questionContainer.append(col)
-           
+    hideLoadingView();    
 
 }
 
 //
 }
-  getListener()  //puts the click listener on the game board
+  handleClick()  //puts the click listener on the game board
 }
 
-
-function getListener(){//click listener for the board
-    const clicker = document.querySelector('#container');
-   
-    clicker.addEventListener('click', function(e){
-      
-     qCat = e.target.classList.value[15]//gets the values of the classes for column and row
-     qNum = e.target.classList.value[20]
-     nClicks = e.target.classList.value[28]//looks to see if the board has been clicked on before
-     
-   if (nClicks === '0') {
-   
-     e.target.innerHTML = gameObj[qCat].clues[qNum].question;
-     e.target.classList.remove('clicks0');
-     e.target.classList.add('clicks1');
-    
-   }
-   if (nClicks === '1') {
-   
-       e.target.innerHTML = gameObj[qCat].clues[qNum].answer;
-       e.target.classList.remove('clicks1');
-     e.target.classList.add('clicks0');
-   
-   }
-   
-   
-    })
-   }
-
-   function loadGameObject(cCount){
-    let questions=[];
-    let answers = [];
-    let QnA = '';
-
-    for(let x = 0; x<6; x++) {
-        y=cCount[x]
-cCount = getCcount(x);
-        // questions[x] = (catRes[x].data.clues[y].question)
-        // answers[x] = (catRes[x].data.clues[y].answer)
-   
-   questions = loadQ(cCount, questions, x);//gets 5 randomly selectios questions
-    answers=loadA(cCount, answers, x);//and the matching answersw
-    gameObj[x] = {title: catRes[x].data.title,
-                clues: [{question: questions[0], answer: answers[0]},
-                        {question: questions[1], answer: answers[1]},
-                        {question: questions[2], answer: answers[2]},
-                        {question: questions[3], answer: answers[3]},
-                        {question: questions[4], answer: answers[4]},
-            
-            
-            ]}
-    }
-
-}
-//loads questions into object
-function loadQ(cCount, questions, idx){
-    for(let x = 0; x<5; x++) {//gets the six catagories for the board
-          let  y=cCount[x];
-                questions[x] = (catRes[idx].data.clues[y].question)
-             //  console.log(questions[x])
-            }
-            return questions
-}
-
-//loads answers into object
-function loadA(cCount, answers, idx){
-  
-    for(let x = 0; x<5; x++) {//gets the six catagories for the board
-          let  y=cCount[x];
-
-                answers[x] = (catRes[idx].data.clues[y].answer)
-            
-            }
-            return answers
-}
-//randomly selects five questions within each catagory
-function getCcount(idx){
-    cCount = [];
-    for(x= 0; x<5; x++) {
-        if(cats[idx].clues_count<5) {
-            console.log('not Enough clues')
-            return;
-        }
-       ran=Math.floor(Math.random()*(cats[idx].clues_count))
-       if(cCount.includes(ran)) {
-        console.log('we had a dupe')
-        x--
-       }
-       else {cCount.push(ran);
-    }
-        
-    }
-    return cCount;
-}
 
 /** Get NUM_CATEGORIES random category from API.
  *
@@ -241,8 +134,48 @@ function getCcount(idx){
  *   ]
  */
 
-function getCategory(catId) {
+
+    function getCategory(catRes) {//loads the game object fuction
+        questionNumber=[];
+        for(let x=0; x<6; x++) {
+       let numOfClues=catRes[x].data.clues_count
+       questionNumber = getRandomQuestion(numOfClues)//looks for a list of five random question and answers
+
+        gameObj[x] = {title: catRes[x].data.title,//initialze game object
+                    clues: 
+                    [
+                               {question: catRes[x].data.clues[questionNumber[0]].question, answer: catRes[x].data.clues[questionNumber[0]].answer},
+                               {question: catRes[x].data.clues[questionNumber[1]].question, answer: catRes[x].data.clues[questionNumber[1]].answer},
+                               {question: catRes[x].data.clues[questionNumber[2]].question, answer: catRes[x].data.clues[questionNumber[2]].answer},
+                               {question: catRes[x].data.clues[questionNumber[3]].question, answer: catRes[x].data.clues[questionNumber[3]].answer},
+                               {question: catRes[x].data.clues[questionNumber[4]].question, answer: catRes[x].data.clues[questionNumber[4]].answer},
+                             ],
+                            
+                        }
+        }  
+   // return gameObj;
+    }
+    
+   
+
+function getRandomQuestion(numOfClues){//randomly selects five questions and answers from the list of catagory ID's
+    let number=[];
+    
+    for(x=0; x<5; x++){
+        nextQuestion = Math.floor(Math.random()*(numOfClues))
+        if (number.includes(nextQuestion)){
+           
+            x--;
+        }else {
+           
+            number.push(nextQuestion)
+        }
+
 }
+return number;
+}
+
+
 
 /** Fill the HTML table#jeopardy with the categories & cells for questions.
  *
@@ -252,7 +185,27 @@ function getCategory(catId) {
  *   (initally, just show a "?" where the question/answer would go.)
  */
 
-async function fillTable() {
+async function fillTable(catagory) {//this is the function that builds the header with the catagorys listed
+        const header = document.querySelector('#container')
+        hContainer = document.createElement('div');
+        hContainer.classList.add('headerBlock')
+        
+         for(x=0; x<6; x++){
+        const square =  document.createElement('div');
+        square.classList.add('catagory');
+        square.innerHTML = catagory[x].data.title;
+        
+        hContainer.append(square)
+        header.append(hContainer)
+         }
+    
+
+
+
+
+
+
+
 }
 
 /** Handle clicking on a clue: show the question or answer.
@@ -264,19 +217,56 @@ async function fillTable() {
  * */
 
 function handleClick(evt) {
-}
+    //click listener for the board
+        const clicker = document.querySelector('#container');
+       
+        clicker.addEventListener('click', function(e){
+          
+         qCat = e.target.classList.value[15]//gets the values of the classes for column and row
+         qNum = e.target.classList.value[20]
+         nClicks = e.target.classList.value[28]//looks to see if the board has been clicked on before
+         
+       if (nClicks === '0') {
+       
+         e.target.innerHTML = gameObj[qCat].clues[qNum].question;
+         e.target.classList.remove('clicks0');//USING CLASSES FOR DATA, IS IT BETTER TO USE THE DATA- STRUCTURE?
+         e.target.classList.add('clicks1');
+        
+       }
+       if (nClicks === '1') {
+       
+           e.target.innerHTML = gameObj[qCat].clues[qNum].answer;
+           e.target.classList.remove('clicks1');
+         e.target.classList.add('clicks0');
+       
+       }
+       
+       
+        })
+       }
+
+
 
 /** Wipe the current Jeopardy board, show the loading spinner,
  * and update the button used to fetch data.
  */
 
 function showLoadingView() {
+loading = document.querySelector('body')
+const loadingElement = document.createElement('div');
+loadingElement.classList.add('spinner')
+loadingElement.innerText='Loading'
+loading.append(loadingElement)
 
 }
 
 /** Remove the loading spinner and update the button used to fetch data. */
 
 function hideLoadingView() {
+hide = document.querySelector('.spinner')
+hide.classList.add('hidden')
+
+
 }
 
 /** Start game:
